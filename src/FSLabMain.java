21eaf84ebae2f9b77ab40c1eae720d970ad78bbf
Brady Farnworth <bafarnwo@marvell.com>
2013-10-08 14:28:31 -0700

    Committing Initial Antfile and main

---
+++ src/FSLabMain.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * @author bafarnwo
+ *
+ */
+public class FSLabMain 
+{
+	/**
+	 * This will initialize the user command line input method
+	 */
+	public void initCommandLine( )
+	{
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		
+		System.out.println( "Hello New World ");
+
+	}
+
+}
