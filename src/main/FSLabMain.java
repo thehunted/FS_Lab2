3ec77985cfe1959b31d78d78e45bce62cf98a4fa
Brady Farnworth <bafarnwo@marvell.com>
2013-10-08 23:20:18 -0700

    Made Changes to the Main file
    Moved the command line parsing to it's own class file

---
+++ src/main/FSLabMain.java
@@ -0,0 +1,33 @@
+package main;
+
+import command.ParseInputCommand;
+
+/**
+ * 
+ */
+
+/**
+ * @author bafarnwo
+ *
+ */
+public class FSLabMain 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args ) 
+	{
+		ParseInputCommand commandPrompt = new ParseInputCommand();
+		
+		System.out.println("Starting Lab 2" );
+		
+		//Here we'll need to parse the metafile
+		
+		//Here we'll need to parse the folders
+		
+		//Init the command prompt and start asking the user for commands
+		commandPrompt.initCommandLine();
+	}
+
+}
